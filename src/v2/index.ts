39234cb24717abe5de461810a2ebe0ca65dcaf57
Tan Yi Long <yilong.tan94@gmail.com>
2020-05-27 10:06:05 +0800

    Add diagnostics check and proper error handling

--- src/v2/index.ts
+++ src/v2/index.ts
@@ -10,24 +10,43 @@ const bitbucketToSlackMap = JSON.parse(process.env.BITBUCKET_TO_SLACK_MAP);
 
 // Main
 const main = async (event: any) => {
-  console.log("data inside", event);
   // TOOD: verification of bitbucket webhook secret
 
+  let response;
   const slackHelper = new SlackHelper(webhookUrl, bitbucketToSlackMap);
-  const { body } = event;
-  if (body) {
-    const payload = JSON.parse(body);
-    // Extract data from webhook
-    const data = BitbucketHelper.getData(payload);
-    // Send slack emssage
-    await slackHelper.sendMessage(payload.eventKey, data);
+  const { headers, body } = event;
+
+  // Diagnostics check
+  if (headers && headers["X-Event-Key"] === "diagnostics:ping") {
+    response = {
+      isBase64Encoded: false,
+      statusCode: 200,
+      body: JSON.stringify({ message: "Diagnostics check successful" }),
+    };
+  } else {
+    try {
+      const payload = JSON.parse(body);
+      // Extract data from webhook
+      const data = BitbucketHelper.getData(payload);
+      // Send slack emssage
+      await slackHelper.sendMessage(payload.eventKey, data);
+
+      response = {
+        isBase64Encoded: false,
+        statusCode: 200,
+        body: JSON.stringify({ message: "Webhook call successful" }),
+      };
+    } catch (err) {
+      console.log("Error: ", err);
+      response = {
+        isBase64Encoded: false,
+        statusCode: 500,
+        body: JSON.stringify({ message: "Webhook call failed" }),
+      };
+    }
   }
 
-  return {
-    isBase64Encoded: false,
-    statusCode: 200,
-    body: JSON.stringify({}),
-  };
+  return response;
 };
 
 export default main;
