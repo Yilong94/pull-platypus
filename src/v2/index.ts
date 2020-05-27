6cf977b787f8d4ae0e16f86d8384468dc317e904
Tan Yi Long <yilong.tan94@gmail.com>
2020-05-27 15:53:31 +0800

    Authenticate with webhook secret

--- src/v2/index.ts
+++ src/v2/index.ts
@@ -10,42 +10,52 @@ const bitbucketToSlackMap = JSON.parse(process.env.BITBUCKET_TO_SLACK_MAP);
 
 // Main
 const main = async (event: any) => {
-  // TOOD: verification of bitbucket webhook secret
-
   let response, message: string;
   const slackHelper = new SlackHelper(webhookUrl, bitbucketToSlackMap);
   const { headers, body } = event;
 
-  // Diagnostics check
-  if (headers && headers["X-Event-Key"] === "diagnostics:ping") {
-    message = "Diagnostics check successful";
+  // Authenticate with webhook secret
+  const hmac = crypto.createHmac("sha256", process.env.WEBHOOK_SECRET);
+  hmac.update(body);
+  if (hmac.digest("hex") !== headers["X-Hub-Signature"]) {
+    message = "Not authorized";
     response = {
       isBase64Encoded: false,
-      statusCode: 200,
+      statusCode: 401,
       body: JSON.stringify({ message }),
     };
   } else {
-    try {
-      const payload = JSON.parse(body);
-      // Extract data from webhook
-      const data = BitbucketHelper.getData(payload);
-
-      // Send slack message
-      if (data) await slackHelper.sendMessage(payload.eventKey, data);
-
-      message = "Webhook call successful";
+    // Diagnostics check
+    if (headers && headers["X-Event-Key"] === "diagnostics:ping") {
+      message = "Diagnostics check successful";
       response = {
         isBase64Encoded: false,
         statusCode: 200,
         body: JSON.stringify({ message }),
       };
-    } catch (err) {
-      message = "Webhook call failed";
-      response = {
-        isBase64Encoded: false,
-        statusCode: 500,
-        body: JSON.stringify({ message }),
-      };
+    } else {
+      try {
+        const payload = JSON.parse(body);
+        // Extract data from webhook
+        const data = BitbucketHelper.getData(payload);
+
+        // Send slack message
+        if (data) await slackHelper.sendMessage(payload.eventKey, data);
+
+        message = "Webhook call successful";
+        response = {
+          isBase64Encoded: false,
+          statusCode: 200,
+          body: JSON.stringify({ message }),
+        };
+      } catch (err) {
+        message = "Webhook call failed";
+        response = {
+          isBase64Encoded: false,
+          statusCode: 500,
+          body: JSON.stringify({ message }),
+        };
+      }
     }
   }
 
