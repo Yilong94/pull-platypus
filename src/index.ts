a297ee9a0568ab8bcdc75fc6de225222327bf51d
Tan Yi Long <yilong.tan94@gmail.com>
2020-06-02 18:17:04 +0800

    Use param store

--- src/index.ts
+++ src/index.ts
@@ -1,69 +1,95 @@
 import crypto from "crypto";
-import dotenv from "dotenv";
 import BitbucketHelper from "./helpers/BitbucketHelper";
 import SlackHelper from "./helpers/SlackHelper";
+import { awsParamStore } from "aws-param-store";
 
-dotenv.config();
-
-// Configuration
-const webhookUrl = process.env.WEBHOOK_URL;
-const bitbucketToSlackMap = JSON.parse(process.env.BITBUCKET_TO_SLACK_MAP);
+enum Param {
+  BITBUCKET_TO_SLACK_MAP = "BITBUCKET_TO_SLACK_MAP",
+  WEBHOOK_URL = "WEBHOOK_URL",
+  WEBHOOK_SECRET = "WEBHOOK_SECRET",
+}
 
 // Main
 const main = async (event: any) => {
-  let response, message: string;
-  const slackHelper = new SlackHelper(webhookUrl, bitbucketToSlackMap);
-  const { headers, body } = event;
+  let message: string;
 
-  // Authenticate with webhook secret
-  const hmacHeader = headers["X-Hub-Signature"].replace("sha256=", "");
-  const hmac = crypto.createHmac("sha256", process.env.WEBHOOK_SECRET);
-  hmac.update(body);
-  const hmacHashed = hmac.digest("hex");
+  // Configuration
+  let bitbucketToSlackMap: string, webhookUrl: string, webhookSecret: string;
+  const ssmPath = process.env.SSM_PATH;
+  try {
+    const parameters = await awsParamStore.getParametersByPath(ssmPath);
+    bitbucketToSlackMap = parameters[Param.BITBUCKET_TO_SLACK_MAP];
+    webhookUrl = parameters[Param.WEBHOOK_URL];
+    webhookSecret = parameters[Param.WEBHOOK_SECRET];
+
+    if (!bitbucketToSlackMap || !webhookUrl || !webhookSecret)
+      throw new Error("Missing environment variables");
+  } catch (err) {
+    message = "Internal Server Error";
+    console.log(err);
+    console.log(message);
+    return {
+      isBase64Encoded: false,
+      statusCode: 500,
+      body: JSON.stringify({ message }),
+    };
+  }
+
+  const { headers, body } = event;
 
   // Diagnostics check
   if (headers && headers["X-Event-Key"] === "diagnostics:ping") {
     message = "Diagnostics check successful";
-    response = {
+    console.log(message);
+    return {
       isBase64Encoded: false,
       statusCode: 200,
       body: JSON.stringify({ message }),
     };
   }
-  // Not authorized
-  else if (hmacHashed !== hmacHeader) {
+
+  // Authenticate with webhook secret
+  const hmacHeader = headers["X-Hub-Signature"].replace("sha256=", "");
+  const hmac = crypto.createHmac("sha256", webhookSecret);
+  hmac.update(body);
+  const hmacHashed = hmac.digest("hex");
+
+  if (hmacHashed !== hmacHeader) {
     message = "Not authorized";
-    response = {
+    console.log(message);
+    return {
       isBase64Encoded: false,
       statusCode: 401,
       body: JSON.stringify({ message }),
     };
   }
-  // Authorized
-  else {
-    try {
-      const payload = JSON.parse(body);
-      // Extract data from webhook
-      const data = BitbucketHelper.getData(payload);
 
-      // Send slack message
-      if (data) await slackHelper.sendMessage(payload.eventKey, data);
+  let response;
+  try {
+    const payload = JSON.parse(body);
+    const slackHelper = new SlackHelper(
+      webhookUrl,
+      JSON.parse(bitbucketToSlackMap)
+    );
+    const data = BitbucketHelper.getData(payload);
 
-      message = "Webhook call successful";
-      response = {
-        isBase64Encoded: false,
-        statusCode: 200,
-        body: JSON.stringify({ message }),
-      };
-    } catch (err) {
-      console.log(err);
-      message = "Webhook call failed";
-      response = {
-        isBase64Encoded: false,
-        statusCode: 500,
-        body: JSON.stringify({ message }),
-      };
-    }
+    // Send slack message
+    if (data) await slackHelper.sendMessage(payload.eventKey, data);
+
+    message = "Webhook call successful";
+    response = {
+      isBase64Encoded: false,
+      statusCode: 200,
+      body: JSON.stringify({ message }),
+    };
+  } catch (err) {
+    console.log(err);
+    message = "Webhook call failed";
+    response = {
+      isBase64Encoded: false,
+      statusCode: 500,
+      body: JSON.stringify({ message }),
+    };
   }
 
   console.log(message);
